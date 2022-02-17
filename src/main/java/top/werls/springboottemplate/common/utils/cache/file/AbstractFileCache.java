d95e2a0d6344c06134d97a49074e2cf2538c3dbd
JiaWei Lee <gsdukbh@gmail.com>
2022-02-17 09:27:14 +0800

    add Captcha 、cache

---
+++ src/main/java/top/werls/springboottemplate/common/utils/cache/file/AbstractFileCache.java
@@ -0,0 +1,17 @@
+package top.werls.springboottemplate.common.utils.cache.file;
+
+import top.werls.core.common.utils.cache.Cache;
+
+import java.io.Serial;
+
+/**
+ * @author leejiawei
+ * @version TODO
+ * @since on  2021/9/30
+ */
+public abstract class AbstractFileCache<K, V> implements Cache<K, V> {
+
+    @Serial
+    private static final long serialVersionUID = -1829581809413030473L;
+
+}
