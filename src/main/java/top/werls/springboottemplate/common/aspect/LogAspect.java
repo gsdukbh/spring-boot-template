3ded5c9fb5acb472f64558248835d80b31746d1f
JiaWei Lee <gsdukbh@gmail.com>
2022-02-22 13:36:29 +0800

    add Captcha 、cache

--- src/main/java/top/werls/springboottemplate/common/aspect/LogAspect.java
+++ src/main/java/top/werls/springboottemplate/common/aspect/LogAspect.java
@@ -2,10 +2,8 @@ package top.werls.springboottemplate.common.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,12 +21,20 @@ public class LogAspect {
 
     @Before("logPointCut()")
     public void before(JoinPoint joinPoint) {
-        log.info("before class: {}", joinPoint.getSignature().getDeclaringType());
-        log.info("before method: {}", joinPoint.getSignature().getName());
+        log.info("before class: {} ,method: {}", joinPoint.getSignature().getDeclaringType(),joinPoint.getSignature().getName());
     }
     @After("logPointCut()")
     public void after(JoinPoint joinPoint) {
-        log.info("after class: {}", joinPoint.getSignature().getDeclaringType());
-        log.info("after method: {}", joinPoint.getSignature().getName());
+        log.info("after class: {} ,method: {}", joinPoint.getSignature().getDeclaringType(),joinPoint.getSignature().getName());
+    }
+    @Around("logPointCut()")
+    public Object doAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result =joinPoint.proceed();
+        log.info("after class: {} ,method: {}, time:{}ms",
+                joinPoint.getSignature().getDeclaringType(),
+                joinPoint.getSignature().getName()
+                ,System.currentTimeMillis()-start);
+        return result;
     }
 }
