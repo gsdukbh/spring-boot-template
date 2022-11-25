00febcba40db812301399984560239915b2b89ff
JiaWei Lee <gsdukbh@gmail.com>
2022-11-25 10:22:50 +0800

    update spring boot 3.0.0

--- src/test/java/top/werls/springboottemplate/SpringBootTemplateApplicationTests.java
+++ src/test/java/top/werls/springboottemplate/SpringBootTemplateApplicationTests.java
@@ -2,12 +2,15 @@ package top.werls.springboottemplate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.werls.springboottemplate.system.entity.SysUser;
 
 @SpringBootTest
 class SpringBootTemplateApplicationTests {
 
     @Test
     void contextLoads() {
+        SysUser user =new SysUser();
+        user.setUsername( "test");
     }
 
 }
